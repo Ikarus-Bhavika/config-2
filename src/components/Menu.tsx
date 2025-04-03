257c5d1ae75abd4bd4f90dd89c5dd5805fe4757b
ikarus-aryan <aryan.verma@ikarus3d.com>
2025-04-03 11:33:30 +0530

    updated css

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -4,8 +4,8 @@ import PriceContainer from "./MenuComponents/PriceContainer";
 
 export default function Menu() {
   return (
-    <div className="hidden lg:w-2/5 xl:[25%] h-[100dvh] lg:flex flex-col justify-center items-center">
-      <div className="w-3/4 py-6 xl:py-10 shadow-md rounded-xl bg-[#F5F8FB] h-[90%] flex p-4 flex-col gap-4">
+    <div className="hidden lg:w-2/5 h-[100dvh] lg:flex flex-col justify-center items-center">
+      <div className="w-3/4 3xl:w-3/5 py-6 xl:py-10 shadow-md rounded-xl bg-[#F5F8FB] h-[90%] flex p-4 flex-col gap-4">
         <MenuHeader/>
         <div className="w-full bg-white grow overflow-y-auto rounded-md flex flex-col gap-2">
         <MenuContainer/>
