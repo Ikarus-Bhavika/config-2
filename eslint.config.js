74507dc25cd32e54461f1f2f9d25bcb9dd5ae5a4
ikarus-aryan <aryan.verma@ikarus3d.com>
2025-04-07 12:44:32 +0530

    updated eslint

--- eslint.config.js
+++ eslint.config.js
@@ -23,7 +23,7 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
-      "@typescript-eslint/no-unused-vars": ["warn", { "varsIgnorePattern": "^_" }]
+      "@typescript-eslint/no-unused-vars": ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
     },
   },
   
