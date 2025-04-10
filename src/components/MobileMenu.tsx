abc7a97503f8c5b60e82f7ec0f46e18204185b5f
ikarus-aryan <aryan.verma@ikarus3d.com>
2025-04-10 12:12:46 +0530

    updated camera animation

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -1,11 +1,13 @@
 import MenuHeader from './MenuComponents/MenuHeader'
 import MenuContainer from './MenuComponents/MenuContainer'
 import PriceContainer from './MenuComponents/PriceContainer'
+import useDataStore from '../store/store'
 
 export default function MobileMenu() {
+  const store = useDataStore();
   return (
     <div style={{boxShadow:'-3px 0px 7px 0px #00000026'}} 
-      className='bg-[#F5F8FB] h-[45%] lg:hidden p-4 lg:p-10 flex flex-col gap-2 overflow-y-auto'>
+      className={`${!store.expandModel && 'pointer-events-none'} select-none bg-[#F5F8FB] h-[45%] lg:hidden p-4 lg:p-10 flex flex-col gap-2 overflow-y-auto`}>
       <MenuHeader />
       <MenuContainer/>
       <hr className='mt-5 mb-1 border border-[#0000002E] w-[95%] mx-auto'/>
