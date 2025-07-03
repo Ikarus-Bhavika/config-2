fb3bcae3d58550f204f88201a9c2c8b4c0c2e31b
Ikarus-Bhavika <bhavika.bhateja@ikarus3d.com>
2025-07-03 13:50:55 +0530

    feat: database

--- src/utils/handleTakeScreenshot.js
+++ src/utils/handleTakeScreenshot.js
@@ -1,13 +1,19 @@
 import html2canvas from 'html2canvas'
 
-export default function handleTakeScreenshot(canvasRef) {
+export default function handleTakeScreenshot(canvasRef,download=true) {
   setTimeout(() => {
     if (canvasRef.current) {
+      console.log(canvasRef.current);
       html2canvas(canvasRef.current).then(function (canvas) {
-        const link = document.createElement('a')
-        link.download = 'screenshot.png'
-        link.href = canvas.toDataURL()
-        link.click()
+        if(download){
+          const link = document.createElement('a')
+          link.download = 'screenshot.png'
+          link.href = canvas.toDataURL()
+          link.click()
+        }
+        else{
+          return canvas.toDataURL()
+        }
       })
     }
   }, 100)
